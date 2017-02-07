dcadc584908bbf7ed2c02e41763b8dabe2f3d899
Jay Sargent <jayssargent@Jays-Mac-Pro.local>
2017-02-07 14:21:58 -0700

    Initial Setup

--- test_1/ViewController.swift
+++ test_1/ViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Anthony: UILabel!
+    
+    @IBOutlet weak var readyMessage: UILabel!
+    
+    var counter = 0
+    
+    @IBAction func start(_ sender: Any) {
+        
+        if counter == 1 {
+            readyMessage.text = "Are you sure?"
+        }
+        
+        if counter == 2 {
+            readyMessage.text = "I don't think you are ..."
+           
+        }
+        if counter == 3 {
+            readyMessage.text = "Here we go ... "
+            
+        }
+        if counter > 3 {
+            readyMessage.text = "I warned you."
+        }
+         counter += 1;
+         print(counter)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
